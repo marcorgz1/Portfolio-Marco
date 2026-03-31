d6f1c8014d0c992329cc057582dc194fd460d5ff
marcorgz1 <marco.valdemoro@gmail.com>
2026-03-31 16:18:53 +0200

    fix: remove github actions deployment method

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+const my_repo = 'Portfolio-Marco';
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/marcorgz1.github.io/',
+  base: `/${my_repo}/`,
   plugins: [
     react(),
     tailwindcss()
