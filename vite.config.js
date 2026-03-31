031cd93616179dcee28a4099abba32d17e673ffd
marcorgz1 <marco.valdemoro@gmail.com>
2026-03-31 17:03:05 +0200

    feat: update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-const my_repo = 'Portfolio-Marco';
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/${my_repo}/`,
   plugins: [
     react(),
     tailwindcss()
